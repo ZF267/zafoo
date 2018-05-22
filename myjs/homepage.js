0cbcaae760faf2ca38c601b205e58243796af09c
zafoo <han.jeffw@gmail.com>
2018-05-22 17:03:23 -0400

    5/22 v1

---
+++ myjs/homepage.js
@@ -0,0 +1,13 @@
+﻿$(window).scroll(function () {
+    headerInit();
+});
+
+function headerInit() {
+    if ($(this).scrollTop() > 0) {
+        $("body").addClass("fixed-header-on");
+    } else {
+        $("body").removeClass("fixed-header-on");
+    }
+}
+
+headerInit();
